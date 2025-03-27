552ae9fbf21de3b891642eda6cc4373a03b358f3
satyam9k <75788300+satyam9k@users.noreply.github.com>
2025-03-27 21:16:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(
         font-size: 1rem;
     }
     .stFileUploader {
-        background-color: #F26B0F;
+        background-color: #7E1891;
         border: 2px dashed #FFB6C1;
         border-radius: 10px;
         padding: 2rem;
